5600e52502177263f2e4bec636d84e07fd9fbfb3
Carlos Benavides <carlos.benavides@capgemini.com>
2023-05-11 14:32:56 +0200

    ci: deploy site to pages on branch push

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,13 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `my-cv`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://cbenavid.github.io/my-cv`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
+  pathPrefix: "/my-cv",
   graphqlTypegen: true,
   plugins: [],
-}
+};
 
-export default config
+export default config;
