82d35840690ab4f859a2a6f4eb483519eb4c948e
Carlos Benavides <carlos.benavides@capgemini.com>
2023-05-14 16:18:53 +0200

    feat: source cv data from file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,11 +3,20 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `my-cv`,
+    author: `Carlos Benavides`,
     siteUrl: `https://cbenavid.github.io/my-cv`,
   },
   pathPrefix: "/my-cv",
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-yaml",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/data`,
+      },
+    },
+  ],
 };
 
 export default config;
